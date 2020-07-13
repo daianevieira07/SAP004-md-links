9000d9ae9dac2ef6210ae0097146e6a61c46dc0c
Daiane Vieira <daiane.vieira07@yahoo.com.br>
2020-07-13 16:26:57 -0300

    README

--- cli.js
+++ cli.js
@@ -1,15 +1,61 @@
 #!/usr/bin/env node
 
-const mdLinks = require('./lib');
+const mdLinks = require('./index');
+const program = require('commander');
+const chalk = require('chalk');
+const package = require("./package.json");
+const path = process.argv[2];
+program.version(package.version);
+program
+  .description(chalk.cyan('Fetch and validate markdown links in files or directories.'))
+  .option('-vl, --validate [true]', 'Returns validated links')
+  .option('-st, --stats [true]', 'Returns link stats')
+const options = {
+  validate: program.validate,
+  stats: program.stats
+};
 
-mdLinks(process.argv[2])
-  .then(array => {
-    if (typeof array === 'undefined') {
-      console.log('Não há links aqui');
+mdLinks(path, {
+    validate: program.validate,
+    stats: program.stats
+  })
+  .then((response) => {
+    if (options.validate) {
+      response.forEach((item) => {
+        console.log(
+          `\nFile: ${chalk.bold(item.file)} \nURL: ${chalk.cyan(
+            item.href
+          )} \nText: ${chalk.bold(item.text)} \nValidate: ${chalk.cyan(
+            item.validate
+          )}`
+        );
+      });
+    } else if (options.stats) {
+      const links = response.map((i) => i.href);
+      const uniqueLinks = new Set(links);
+      const brokenLinks = response.filter((i) => i.validate !== '200 OK');
+      console.log(
+        chalk.bold(
+          `\nTotal links: ${chalk.cyan(
+            response.length
+          )}\nUnique links: ${chalk.cyan(
+            uniqueLinks.size
+          )}\nBroken links: ${chalk.cyan(brokenLinks.length)}
+          `
+        )
+      );
     } else {
-      array.forEach(obj => {
-        console.log(`text: ${obj.text} | href: ${obj.href} | file: ${obj.file} `)
+      response.forEach((item) => {
+        console.log(
+          `\nFile: ${chalk.bold(item.file)} \nURL: ${chalk.cyan(
+            item.href
+          )} \nText: ${chalk.bold(item.text)}`
+        );
       });
     }
   })
-  .catch(error => console.log(error));
+  .catch((err) => {
+    console.log(err);
+  });
+
+program.parse(process.argv);
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,21 +1,10 @@
-/* const mdLinks = require('../');
+const mdLinks = require('../');
 
 
 describe('mdLinks', () => {
 
   it('should ...', () => {
     console.log('FIX ME!');
-  });
-
-}); */
-
-import mdLinks from './index'
-
-/* const mdLinks = require('../'); */
-
-describe('mdLinks', () => {
-  it('should return 5', () => {
-    expect(5).toBe(5)
   });
 
 });
